bc022a0f59f3a7d3ea0e7a0ee08c6dccd07b47de
Oliver Goodman <oliver.goodman07+git@gmail.com>
2016-04-29 06:29:00 +0000

    Removes tabs.

--- js/ws_demo.js
+++ js/ws_demo.js
@@ -9,47 +9,47 @@ function(when, rpc, publisher) {
     var st = document.getElementById('status');
 
     serv.onopen = function() {
-	st.style.color = 'green';
-	st.textContent = 'connected';
+        st.style.color = 'green';
+        st.textContent = 'connected';
     };
     serv.onclose = function() {
-	bound = false;
-	st.style.color = 'red';
-	st.textContent = 'disconnected';
+        bound = false;
+        st.style.color = 'red';
+        st.textContent = 'disconnected';
     };
 
     // ------------------------------------------------------------
 
 
     function bindInput(elem, model, name) {
-	elem.addEventListener('change', function(e) {
-	    model.set(name, elem.value);
-	});
-	model.subscribe('change', function(e, info) {
-	    if (info.key == name) {
-		elem.value = info["new"];
-	    }
-	});
-	model.get(name).done(function(value) {
-	    elem.value = value || '';
-	});
+        elem.addEventListener('change', function(e) {
+            model.set(name, elem.value);
+        });
+        model.subscribe('change', function(e, info) {
+            if (info.key == name) {
+                elem.value = info["new"];
+            }
+        });
+        model.get(name).done(function(value) {
+            elem.value = value || '';
+        });
     };
     window.bindInput = bindInput;
     
     function logEvent(event, info) {
-	console.log('notified:', event, info);
+        console.log('notified:', event, info);
     };
     window.logEvent = logEvent;
 
     // Example hook class
     function HookCls(val) {
-	this.val = val;
+        this.val = val;
     };
     HookCls.prototype.ext_encoding = function() {
-	return {name: 'HookCls', args: [this.val]};
+        return {name: 'HookCls', args: [this.val]};
     };
     rpc.WSServer.prototype.hooks['HookCls'] = function(server, args) {
-	return new HookCls(args[0]);
+        return new HookCls(args[0]);
     };
 
     window.HookCls = HookCls;
@@ -60,26 +60,26 @@ function(when, rpc, publisher) {
     // set up binding.
     var input = document.getElementById('input1');
     function doBind() {
-	if (!bound) {
-	    window.model = new rpc.Proxy(serv, 'shared');
-	    window.model.addMethod('get');
-	    window.model.addMethod('set');
-	    window.model.addMethod('notify');
-	    window.model.addMethod('subscribers');
+        if (!bound) {
+            window.model = new rpc.Proxy(serv, 'shared');
+            window.model.addMethod('get');
+            window.model.addMethod('set');
+            window.model.addMethod('notify');
+            window.model.addMethod('subscribers');
 
-	    bindInput(input, model, 'name');
-	    bound = true;
-	} else {
-	    console.log('already bound');
-	}
+            bindInput(input, model, 'name');
+            bound = true;
+        } else {
+            console.log('already bound');
+        }
     }
 
     // Test for passing proxy to server.
     function Squarer() { // for Maud
     }
     Squarer.prototype.square = function(n) {
-	console.log('squaring', n);
-	return n * n;
+        console.log('squaring', n);
+        return n * n;
     };
 
     var sqc = new rpc.Proxy(serv, 'sqcaller');
@@ -90,24 +90,24 @@ function(when, rpc, publisher) {
     var pp = new rpc.Proxy(serv, 'pub', 'browser');
 
     function sendProxyToServer() {
-	serv.obj['squarer'] = new Squarer();
-	console.log('serv.obj.squarer', serv.obj['squarer']);
-	var sq = new rpc.Proxy(serv, 'squarer', 'browser');
-	sqc.useSquarer(sq, 3).done(rlog);
+        serv.obj['squarer'] = new Squarer();
+        console.log('serv.obj.squarer', serv.obj['squarer']);
+        var sq = new rpc.Proxy(serv, 'squarer', 'browser');
+        sqc.useSquarer(sq, 3).done(rlog);
     }
 
     function testBoundMethod() {
-	window.shget = makeBoundMethod(serv, 'shared', 'get', 'server', true);
-	console.log('doing shget("name")', shget('name').done(rlog));
-	serv.obj['squarer'] = new Squarer();
-	window.square = makeBoundMethod(serv, 'squarer', 'square', 'browser');
-	console.log('square(6)', square(6));
+        window.shget = makeBoundMethod(serv, 'shared', 'get', 'server', true);
+        console.log('doing shget("name")', shget('name').done(rlog));
+        serv.obj['squarer'] = new Squarer();
+        window.square = makeBoundMethod(serv, 'squarer', 'square', 'browser');
+        console.log('square(6)', square(6));
     }
 
     function testSubscribeToClient() {
-	sqc.subscribeToClient(pp).done(function() {
-	    serv.obj['pub'].notify('event', 'foobar');
-	});
+        sqc.subscribeToClient(pp).done(function() {
+            serv.obj['pub'].notify('event', 'foobar');
+        });
     }
 
     window.Publisher = Publisher;
