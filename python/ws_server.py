86bf0df41bc194bcf0d7c9779840029519e42624
Oliver Goodman <oag@optusnet.com.au>
2017-04-08 12:21:25 +1000

    Makes ws_server.py default to silent.

--- python/ws_server.py
+++ python/ws_server.py
@@ -6,6 +6,7 @@ TODO: reimplement using pure eventlet websocket module.
 """
 
 import eventlet
+import sys
 import weakref
 from serf.eventlet_thread import EventletThread
 from serf.ws_transport import WSTransport
@@ -16,34 +17,39 @@ from serf.rpc_handler import RPCHandler
 SINGLETON_MODEL = Model()
 
 class SquareCaller(object):
-    def __init__(self, vat, oid):
+    def __init__(self, vat, oid, verbose=False):
         self.vat = weakref.ref(vat)
         self.oid = oid
+        self.verbose = verbose
     def useSquarer(self, sq, n):
         r = sq.square(n)
-        print 'square of %s is %s' % (n, r)
+        if self.verbose:
+            print 'square of %s is %s' % (n, r)
         return r
     def callSquare(self, square, n):
         r = square(n)
-        print 'square of %s is %s' % (n, r)
+        if self.verbose:
+            print 'square of %s is %s' % (n, r)
         return r
     def subscribeToClient(self, prx):
         method = BoundMethod(self.vat(), self.oid, 'onClientEvent', 'server')
         prx.subscribe('event', method)
     def onClientEvent(self, ev, info):
-        print 'got event', ev, info
+        if self.verbose:
+            print 'got event', ev, info
 
 class Ping(object):
     def ping(self):
         return 'pong'
 
 def handle(transport):
+    verbose = '-v' in sys.argv
     thread = EventletThread()
     thread.callFromThread = thread.call
-    handler = RPCHandler(transport, {}, t_model=thread, verbose=True)
+    handler = RPCHandler(transport, {}, t_model=thread, verbose=verbose)
     handler.provide('shared', SINGLETON_MODEL)
     handler.provide('private', Model())
-    handler.provide('sqcaller', SquareCaller(handler, 'sqcaller'))
+    handler.provide('sqcaller', SquareCaller(handler, 'sqcaller', verbose))
     handler.provide('ping', Ping())
     transport.handle()
 
